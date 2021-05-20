d96f0b0a7087f2b6fd293803c76a0cede71b5080
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-20 11:49:57 -0700

    Extract greeting text into a function and renamed greeting function to welcome.

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void printgreeting() {
+void printWelcome() {
     printf("Welcome to the Solar System!\n");
     printf("There are 9 planets to explore.\n");
 }
 
-int main(int argc, char** argv) {
-    printgreeting();
+void printGreeting() {
     printf("What is your name?\n");
     char name[32];
     fgets(name, 32, stdin);
     name[strcspn(name, "\n")] = 0;
     printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
+}
+
+int main(int argc, char** argv) {
+    printWelcome();
+    printGreeting();
     printf("Let's go on an adventure!\n");
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     char chooseRandomPlanet[2];
