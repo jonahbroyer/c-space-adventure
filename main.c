c83cd310c81dc2f89ad3c9fc88fbe9d162f1d981
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-20 11:31:13 -0700

    Remove newline from input.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // C Space Adventure
 // Author: Jonah Broyer
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
     printf("Welcome to the Solar System!\n");
@@ -8,8 +9,9 @@ int main(int argc, char** argv) {
     printf("What is your name?\n");
     char name[32];
     fgets(name, 32, stdin);
+    name[strcspn(name, "\n")] = 0;
     printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
-    printf("Let's go on an adventure!");
+    printf("Let's go on an adventure!\n");
     printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
     char randomPlanet[2];
     fgets(randomPlanet, 32, stdin);
