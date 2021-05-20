22ddbda0c22f1416f2b49e1ee8e7b66e6001ddd3
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-20 11:21:37 -0700

    Generate basic output and capture user input.

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 // C Space Adventure
+// Author: Jonah Broyer
+#include <stdio.h>
 
-int
-main(int argc, char *argv[])
-{
-    /* code */
-    return 0;
+int main(int argc, char** argv) {
+    printf("Welcome to the Solar System!\n");
+    printf("There are 9 planets to explore.\n");
+    printf("What is your name?\n");
+    char name[32];
+    fgets(name, 32, stdin);
+    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
+    printf("Let's go on an adventure!");
+    printf("Shall I randomly choose a planet for you to visit? (Y or N)\n");
+    char randomPlanet[2];
+    fgets(randomPlanet, 32, stdin);
+    printf("Traveling to Pluto...\n");
+    printf("Arrived at Pluto, it's very cold here.\n");
 }
+
