3390ce4eb9121c17ae00a68c3e97cebde01bd1b2
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-28 08:47:07 -0700

    Added a header file for all of the functions.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 // Author: Jonah Broyer
 #include <stdio.h>
 #include <string.h>
+#include "functions.h"
 
 void printWelcome();
 void printGreeting();
@@ -35,28 +36,4 @@ int main(int argc, char** argv) {
     }
 
     return 0;
-}
-
-void printWelcome() {
-    printf("Welcome to the Solar System!\n");
-    printf("There are 9 planets to explore.\n");
-}
-
-void printGreeting() {
-    printf("What is your name?\n");
-    char name[32];
-    fgets(name, 32, stdin);
-    name[strcspn(name, "\n")] = 0;
-    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
-    printf("Let's go on an adventure!\n");
-}
-
-void printRandomPlanet() {
-    printf("Traveling to Pluto...\n");
-    printf("Arrived at Pluto, it's very cold here.\n");
-}
-
-void printChosenPlanet() {
-    printf("Traveling to Jupiter...\n");
-    printf("Arrived at Jupiter. A gas giant, with a noticeable red spot.\n");
 }
