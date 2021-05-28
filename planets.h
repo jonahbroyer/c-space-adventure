bfe6fa62f3b180f767f05c9d7a0b802fc6663fed
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-28 09:12:01 -0700

    added a struct for the planets data

---
+++ planets.h
@@ -0,0 +1,14 @@
+// data about the planets in the solar system
+#include <stdio.h>
+
+typedef struct solar_system {
+    char *mercury;
+    char *venus;
+    char *earth;
+    char *mars;
+    char *jupiter;
+    char *saturn;
+    char *uranus;
+    char *neptune;
+    char *pluto;
+};
