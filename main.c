e5be5020ec9c73405c85d814d0fe2b0405105dfd
Jonah Broyer <jkbsk8er@yahoo.com>
2021-05-28 08:39:19 -0700

    Moved main closer to the top of main.c

--- main.c
+++ main.c
@@ -3,29 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void printWelcome() {
-    printf("Welcome to the Solar System!\n");
-    printf("There are 9 planets to explore.\n");
-}
-
-void printGreeting() {
-    printf("What is your name?\n");
-    char name[32];
-    fgets(name, 32, stdin);
-    name[strcspn(name, "\n")] = 0;
-    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
-    printf("Let's go on an adventure!\n");
-}
-
-void printRandomPlanet() {
-    printf("Traveling to Pluto...\n");
-    printf("Arrived at Pluto, it's very cold here.\n");
-}
-
-void printChosenPlanet() {
-    printf("Traveling to Jupiter...\n");
-    printf("Arrived at Jupiter. A gas giant, with a noticeable red spot.\n");
-}
+void printWelcome();
+void printGreeting();
+void printRandomPlanet();
+void printChosenPlanet();
 
 int main(int argc, char** argv) {
     printWelcome();
@@ -54,4 +35,28 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+void printWelcome() {
+    printf("Welcome to the Solar System!\n");
+    printf("There are 9 planets to explore.\n");
+}
+
+void printGreeting() {
+    printf("What is your name?\n");
+    char name[32];
+    fgets(name, 32, stdin);
+    name[strcspn(name, "\n")] = 0;
+    printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Alexa.\n", name);
+    printf("Let's go on an adventure!\n");
+}
+
+void printRandomPlanet() {
+    printf("Traveling to Pluto...\n");
+    printf("Arrived at Pluto, it's very cold here.\n");
+}
+
+void printChosenPlanet() {
+    printf("Traveling to Jupiter...\n");
+    printf("Arrived at Jupiter. A gas giant, with a noticeable red spot.\n");
 }
